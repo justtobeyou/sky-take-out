ff81239ee5cafacd690bd5d3e35bb6afa938e7c2
rongzhi <16622810365@163.com>
2025-02-23 20:25:17 +0800

    菜品管理

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,10 +1,17 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 通用接口
@@ -13,8 +20,31 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/common")
 @Slf4j
 public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
     public Result<String> upload(MultipartFile file){
         log.info("文件上传：{}",file);
-        return Result.success();
+
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件名的后缀
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString()+extension;
+
+            String filePath= aliOssUtil.upload(file.getBytes(),objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败:{}",e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,42 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
+
+public interface DishService {
+
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据id查询菜品
+     * @return
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 修改菜品基本信息和对应的口味信息
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
+}
